47ec1e5fb0e218dad1592cd80619a4b317b6230b
dmunagapati <dmunagapati@gmail.com>
2025-03-16 20:29:29 -0400

    trying to fix the images issue by restoring old version without accuracy

--- stock-market-frontend/src/App.jsx
+++ stock-market-frontend/src/App.jsx
@@ -1,22 +1,21 @@
-// src/App.jsx
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import StockSearch from "./StockSearch";
 import StockCharts from "./StockCharts";
-import StockAccuracy from "./StockAccuracy"; // Import the new StockAccuracy component
-import "./App.css";
+import "./App.css"; 
+
+
+
 
 function App() {
   const [images, setImages] = useState([]);
-  const [accuracy, setAccuracy] = useState({}); // New state to hold accuracy data
-  console.log("Accuracy Prop:", accuracy);
 
   return (
     <div className="container">
       <h1 className="title">Stock Market Analysis</h1>
       <p className="subtitle">Get real-time stock insights with interactive charts</p>
-      <StockSearch setImages={setImages} setAccuracy={setAccuracy} />
+      <StockSearch setImages={setImages} />
       <StockCharts images={images} />
-      <StockAccuracy accuracy={accuracy} />
     </div>
   );
 }
